9824f9e3e15b69decdaed642d4f190c8e70dfff1
lukman aul <archer.lukman@gmail.com>
2022-02-09 21:44:49 +0700

    create value validator of array
    get conclusion of several checking of method

---
+++ ValueValidator.js
@@ -0,0 +1,10 @@
+const dataX =(x) => x > 5 // need x argument
+const dataY = (y) => y < 10 // need y argument
+
+let cek = [dataX, dataY] //===> accumulate value TRUE OR FALSE, depends on value input whether or not meet dataX and dataY
+let flag = true
+for (const validator of cek) { // ----> loop thru cek array
+    console.log(validator(4)) // ----> logging each result
+    flag = flag && validator(4) // ----> last result
+}
+console.log(flag)
